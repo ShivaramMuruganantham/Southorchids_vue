ce49f1de7f7b9a6cb9147e5b91abc0da977aa603
Shivaram <144917260+ShivaramMuruganantham@users.noreply.github.com>
2025-01-24 14:34:26 +0530

    Responsive order history page

--- src/pages/orderHistory.vue
+++ src/pages/orderHistory.vue
@@ -1,37 +1,40 @@
 <template>
   <div class="p-7">
-    <div class="relative flex justify-center">
-        <p class="absolute text-3xl left-[50px]" @click="$router.go(-1)"><i class="icon ion-md-arrow-back"></i></p>
+    <div class="relative flex justify-between">
+        <p class="text-3xl " @click="$router.go(-1)"><i class="icon ion-md-arrow-back"></i></p>
         <p class="text-2xl text-center underline">Order History</p>
+        <p></p>
     </div>
-    <div v-for="order in userOrder" :key="order.id" class="p-3 border w-[550px] m-10">
-        <div class="flex items-center justify-between">
-            <div>
-                <p>Order ID : <span>#{{ order.id }}</span></p>
-                <p>Order Date : <span>{{ order.order_date }}</span></p>
-            </div>
-            <div>
-                <p>Payment : <span>{{ order.payment_status }}</span> </p>
-                <p>Delivery status : <span>{{ order.delivery_status }}</span> </p>
-                
-            </div>
-        </div>
-        <div v-for="item in order.items" :key="item.product_name" class="flex items-center p-4 my-4 border">
-            <img :src="item.product_image" alt="" class="h-16">
-            <div class="flex items-center justify-between w-full p-2">
+    <div v-for="order in userOrder" :key="order.id" class="my-14">
+        <div v-if="order.payment_status == 'Completed'" class="p-3 border w-[550px] m-10 max-md:w-full max-md:m-0 max-sm:w-full max-sm:m-0">
+            <div class="flex items-center justify-between">
                 <div>
-                    <p>{{ item.product_name }}</p>
-                    <p>{{ item.product_brand }}</p>
-                    <p>x {{ item.quantity }}</p>
+                    <p class="max-sm:text-xs">Order ID : <span class="max-sm:font-bold">#{{ order.id }}</span></p>
+                    <p class="max-sm:text-xs">Order Date : <span class="max-sm:font-bold">{{ order.order_date }}</span></p>
                 </div>
-                <div class="text-end">
-                    <p class=""><span>&#8377;</span> {{ (item.product_price * item.quantity).toFixed(2) }}</p>
+                <div>
+                    <p class="max-sm:text-xs">Payment : <span class="max-sm:font-bold">{{ order.payment_status }}</span> </p>
+                    <p class="max-sm:text-xs">Delivery status : <span class="max-sm:font-bold">{{ order.delivery_status }}</span> </p>
+                    
                 </div>
             </div>
-        </div>
-        <div class="flex justify-between px-5 py-3 text-xl font-bold text-white bg-yellow-400">
-            <p>Total price :</p>
-            <p><span>&#8377;</span> {{ order.total_amount.toFixed(2) }}</p>
+            <div v-for="item in order.items" :key="item.product_name" class="flex items-center p-4 my-4 border max-sm:p-2 max-sm:my-2">
+                <img :src="item.product_image" alt="" class="h-16 ">
+                <div class="flex items-center justify-between w-full p-2">
+                    <div>
+                        <p class="max-sm:text-sm">{{ item.product_name }}</p>
+                        <p class="max-sm:text-sm">{{ item.product_brand }}</p>
+                        <p class="max-sm:text-sm">x {{ item.quantity }}</p>
+                    </div>
+                    <div class="text-end">
+                        <p class="max-sm:text-sm"><span>&#8377;</span> {{ (item.product_price * item.quantity).toFixed(2) }}</p>
+                    </div>
+                </div>
+            </div>
+            <div class="flex justify-between px-5 py-3 text-xl font-bold text-white bg-yellow-400 max-sm:text-lg">
+                <p>Total price :</p>
+                <p><span>&#8377;</span> {{ order.total_amount.toFixed(2) }}</p>
+            </div>
         </div>
     </div>
   </div>
@@ -56,10 +59,13 @@ export default {
             })
             .then(response => response.json())
             .then(data => {
-                console.log(data);
                 this.userOrder = data.data;
             })
         },
+    },
+
+    computed: {
+
     },
     
     created() {
