ea19e99c700725730dfc322ad613849eefe6e1e9
Shivaram <144917260+ShivaramMuruganantham@users.noreply.github.com>
2025-01-24 14:33:05 +0530

    Responsive about page

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,37 +1,37 @@
 <template>
     <div>
         <Navbar></Navbar>
-        <div class="p-5 pt-[98px]">
-            <p class="px-5 pt-5 text-3xl font-bold">Our Mission</p>
-            <div class="p-5 mt-4">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, rem quaerat? Error dolorum natus laborum, illo animi quo hic praesentium et deleniti assumenda quaerat voluptatem recusandae magni id eos architecto? Nostrum illum eaque eos. Error, quos libero nesciunt sit fugiat corrupti sapiente fugit officiis animi! Numquam, rerum repellendus? Blanditiis exercitationem nemo aliquam qui consequatur aspernatur modi vel error accusantium unde, doloribus voluptatibus consequuntur mollitia laudantium libero, eaque ullam perspiciatis veritatis cumque amet aperiam, quis dolores. Eius voluptatibus reiciendis totam minima animi doloribus possimus impedit ullam numquam alias sed quae, nam autem atque consectetur quos ipsum ab eum vel saepe ea?</p>
-                <img src="/public/images/banners/photo-1634762044247-dfbf7bdcd458.jpg" alt="" class="p-5 w-[80%] h-96 mx-auto my-5">
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit cupiditate asperiores non sit reiciendis excepturi quibusdam explicabo nesciunt sunt necessitatibus ullam, rem corrupti labore! A quod, vero qui eveniet ex, hic, adipisci eum maxime officia accusantium quibusdam quisquam cum labore alias aliquam commodi? Fugiat et neque itaque sint sed explicabo recusandae autem harum expedita incidunt eaque fugit quasi architecto at facilis, accusamus modi. Quos est illum culpa recusandae consequatur reprehenderit? Non fugiat consequatur saepe explicabo praesentium assumenda perferendis voluptatibus ea sunt id, accusantium libero exercitationem in earum tempora officia voluptatem obcaecati iure ut sequi ullam vel vero, magni reiciendis. Eaque voluptas ipsa cupiditate, repudiandae saepe excepturi eligendi necessitatibus sit dolorum reprehenderit commodi ratione expedita sequi consequatur? Ut excepturi cupiditate nam asperiores rem itaque commodi quidem consectetur, mollitia assumenda dolore soluta laboriosam labore, delectus similique! Repellat, laborum eveniet odio a omnis voluptate, dolor ab illo, aliquid quo quia. Dignissimos vel id aliquam voluptate obcaecati ipsa tempore sit perspiciatis doloremque veritatis laboriosam ullam enim sapiente alias quo delectus ex amet magnam facilis quibusdam, quam placeat? Incidunt perspiciatis delectus quae consectetur esse excepturi voluptatem aperiam in sed quam, ipsa sequi quisquam praesentium debitis a eius. Beatae ea unde rem perspiciatis harum tenetur adipisci.</p>
-                <div class="flex justify-around p-5 my-3 ">
-                    <img src="/public/images/banners/bees.jpg" alt="" class="">
-                    <img src="/public/images/banners/honeybee box.jpg" alt="">
+        <div class="p-5 pt-[98px] max-md:p-2 max-md:pt-20 max-sm:p-2 max-sm:pt-20">
+            <p class="px-5 pt-5 text-3xl font-bold max-md:text-2xl max-md:px-2 max-sm:px-2">Our Mission</p>
+            <div class="p-5 mt-4 max-md:mt-2 max-md:p-2 max-sm:mt-2 max-sm:p-3">
+                <p class=" max-sm:text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, rem quaerat? Error dolorum natus laborum, illo animi quo hic praesentium et deleniti assumenda quaerat voluptatem recusandae magni id eos architecto? Nostrum illum eaque eos. Error, quos libero nesciunt sit fugiat corrupti sapiente fugit officiis animi! Numquam, rerum repellendus? Blanditiis exercitationem nemo aliquam qui consequatur aspernatur modi vel error accusantium unde, doloribus voluptatibus consequuntur mollitia laudantium libero, eaque ullam perspiciatis veritatis cumque amet aperiam, quis dolores. Eius voluptatibus reiciendis totam minima animi doloribus possimus impedit ullam numquam alias sed quae, nam autem atque consectetur quos ipsum ab eum vel saepe ea?</p>
+                <img src="/public/images/banners/photo-1634762044247-dfbf7bdcd458.jpg" alt="" class="p-5 w-[80%] h-96 mx-auto my-5 max-sm:w-full max-sm:h-60 max-sm:p-0">
+                <p class="max-sm:text-sm max-sm:mt-2">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit cupiditate asperiores non sit reiciendis excepturi quibusdam explicabo nesciunt sunt necessitatibus ullam, rem corrupti labore! A quod, vero qui eveniet ex, hic, adipisci eum maxime officia accusantium quibusdam quisquam cum labore alias aliquam commodi? Fugiat et neque itaque sint sed explicabo recusandae autem harum expedita incidunt eaque fugit quasi architecto at facilis, accusamus modi. Quos est illum culpa recusandae consequatur reprehenderit? Non fugiat consequatur saepe explicabo praesentium assumenda perferendis voluptatibus ea sunt id, accusantium libero exercitationem in earum tempora officia voluptatem obcaecati iure ut sequi ullam vel vero, magni reiciendis. Eaque voluptas ipsa cupiditate, repudiandae saepe excepturi eligendi necessitatibus sit dolorum reprehenderit commodi ratione expedita sequi consequatur? Ut excepturi cupiditate nam asperiores rem itaque commodi quidem consectetur, mollitia assumenda dolore soluta laboriosam labore, delectus similique! Repellat, laborum eveniet odio a omnis voluptate, dolor ab illo, aliquid quo quia. Dignissimos vel id aliquam voluptate obcaecati ipsa tempore sit perspiciatis doloremque veritatis laboriosam ullam enim sapiente alias quo delectus ex amet magnam facilis quibusdam, quam placeat? Incidunt perspiciatis delectus quae consectetur esse excepturi voluptatem aperiam in sed quam, ipsa sequi quisquam praesentium debitis a eius. Beatae ea unde rem perspiciatis harum tenetur adipisci.</p>
+                <div class="flex justify-around p-5 my-3 max-md:p-2 max-md:flex-col max-md:gap-5 max-sm:flex-col max-sm:gap-3 max-sm:p-0 max-sm:my-5">
+                    <img src="/public/images/banners/bees.jpg" alt="" class="max-md:w-[80%] max-md:mx-auto">
+                    <img src="/public/images/banners/honeybee box.jpg" alt="" class="max-md:w-[80%] max-md:mx-auto">
                 </div>
-                <p class="p-1">Lorem ipsum, dolor sit amet consectetur adipisicing elit. In beatae eius, laboriosam vitae labore ex dolor voluptate modi adipisci tempore quo? Numquam nobis autem nesciunt officiis quae, non ut praesentium corporis obcaecati consectetur itaque qui, aliquam at enim iusto deleniti veniam? Ex natus asperiores non libero quisquam, provident accusamus alias inventore at cum, molestias earum rem aliquid animi expedita aut ipsum dolorem modi laudantium porro. Enim ex molestias aliquam officiis necessitatibus culpa illum tempore repellendus et incidunt pariatur omnis, unde porro nostrum cumque voluptatum eveniet eos possimus.,</p>
-                <p class="p-1"> Quo doloribus nemo ut aspernatur ducimus, consequuntur hic culpa at, eaque commodi animi reprehenderit aliquam earum! Suscipit ullam non sit praesentium ipsum officiis eligendi inventore assumenda, facilis cupiditate facere blanditiis quam. Recusandae harum quo tempora. Commodi nulla tempore quas architecto, reiciendis, adipisci officia nihil impedit deserunt expedita facilis atque incidunt magni. Voluptatum iste totam magnam dicta. Explicabo neque repudiandae ipsa at tempore quae quod veniam beatae commodi laudantium impedit suscipit ad unde aspernatur, mollitia ducimus? Officiis perspiciatis labore omnis tenetur amet expedita error, nostrum veritatis asperiores assumenda, eaque officia reprehenderit porro a. Dolor, ratione veritatis. Architecto officia nam hic soluta fuga numquam sunt modi ad illum magni perferendis, reiciendis quae officiis magnam libero.</p>
+                <p class="p-1 max-sm:text-sm">Lorem ipsum, dolor sit amet consectetur adipisicing elit. In beatae eius, laboriosam vitae labore ex dolor voluptate modi adipisci tempore quo? Numquam nobis autem nesciunt officiis quae, non ut praesentium corporis obcaecati consectetur itaque qui, aliquam at enim iusto deleniti veniam? Ex natus asperiores non libero quisquam, provident accusamus alias inventore at cum, molestias earum rem aliquid animi expedita aut ipsum dolorem modi laudantium porro. Enim ex molestias aliquam officiis necessitatibus culpa illum tempore repellendus et incidunt pariatur omnis, unde porro nostrum cumque voluptatum eveniet eos possimus.,</p>
+                <p class="p-1 max-sm:text-sm"> Quo doloribus nemo ut aspernatur ducimus, consequuntur hic culpa at, eaque commodi animi reprehenderit aliquam earum! Suscipit ullam non sit praesentium ipsum officiis eligendi inventore assumenda, facilis cupiditate facere blanditiis quam. Recusandae harum quo tempora. Commodi nulla tempore quas architecto, reiciendis, adipisci officia nihil impedit deserunt expedita facilis atque incidunt magni. Voluptatum iste totam magnam dicta. Explicabo neque repudiandae ipsa at tempore quae quod veniam beatae commodi laudantium impedit suscipit ad unde aspernatur, mollitia ducimus? Officiis perspiciatis labore omnis tenetur amet expedita error, nostrum veritatis asperiores assumenda, eaque officia reprehenderit porro a. Dolor, ratione veritatis. Architecto officia nam hic soluta fuga numquam sunt modi ad illum magni perferendis, reiciendis quae officiis magnam libero.</p>
             </div>
             
-            <div class="p-5">
-                <p class="text-3xl font-bold">Related articles or posts</p>
-                <div class="flex justify-around py-5 mt-4">
-                    <div class="border rounded">
-                        <img src="/public/images/banners/tea-festival2.jpg" alt="" class="h-48 rounded-t w-80">
-                        <p class="p-2 font-semibold">Title</p>
+            <div class="p-5 max-md:p-2 max-sm:p-2">
+                <p class="text-3xl font-bold max-md:text-2xl max-sm:text-xl">Related articles or posts</p>
+                <div class="flex justify-around py-5 mt-4 max-md:gap-5 max-sm:flex-col max-sm:gap-5 max-sm:py-3">
+                    <div class="border rounded shadow">
+                        <img src="/public/images/banners/tea-festival2.jpg" alt="" class="h-48 rounded-t w-80 max-md:h-40 max-sm:w-full">
+                        <p class="pt-2 px-2 font-semibold">Title</p>
                         <p class="p-2 text-sm ">Author</p>
                     </div>
-                    <div class="border rounded">
-                        <img src="/public/images/banners/hills.webp" alt="" class="h-48 rounded-t w-80">
-                        <p class="p-2 font-semibold">Title</p>
-                        <p class="px-2 text-sm">Author</p>
+                    <div class="border rounded shadow">
+                        <img src="/public/images/banners/hills.webp" alt="" class="h-48 rounded-t w-80 max-md:h-40 max-sm:w-full">
+                        <p class="pt-2 px-2 font-semibold">Title</p>
+                        <p class="p-2 text-sm">Author</p>
                     </div>
-                    <div class="border rounded">
-                        <img src="/public/images/banners/tea-festival2.jpg" alt="" class="h-48 rounded-t w-80">
-                        <p class="p-2 font-semibold">Title</p>
-                        <p class="px-2 text-sm">Author</p>
+                    <div class="border rounded shadow">
+                        <img src="/public/images/banners/tea-festival2.jpg" alt="" class="h-48 rounded-t w-80 max-md:h-40 max-sm:w-full">
+                        <p class="pt-2 px-2 font-semibold">Title</p>
+                        <p class="p-2 text-sm">Author</p>
                     </div>
                 </div>
             </div>
