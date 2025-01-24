b4a60112c2d58a8815048f76b9e439d7f6453f3c
Shivaram <144917260+ShivaramMuruganantham@users.noreply.github.com>
2025-01-24 14:33:30 +0530

    Responsive home page

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -2,139 +2,141 @@
     <div>
         <Navbar></Navbar>
 
-        <div class="p-5 pt-[98px]">
-            <div class="bg-[url('/images/banners/box.jpg')] bg-cover bg-center h-[490px] rounded-xl px-10 flex items-center text-white">
-                <div>
-                    <p class="text-4xl font-sequelHead">BeeKeepers Natural.</p>
-                    <p class="pt-2 w-[600px] text-lg">Subheading that sets up context, shares more info about the website, or generally gets people psyched to keep scrolling.</p>
-                    <button class="bg-yellow-500 px-3 py-0.5 text-xl mt-5 rounded">SignUp</button>
+        <div class="p-5 pt-[98px] max-md:p-3 max-md:pt-24 max-sm:p-2 max-sm:pt-20">
+            <div class="relative bg-[url('/images/banners/box.jpg')] bg-cover bg-center h-[490px] rounded-xl px-10 flex items-center text-white max-md:h-96 max-sm:h-96 max-sm:px-2">
+                <div class="bg-black bg-opacity-30 rounded-xl absolute top-0 left-0 h-full w-full"></div>
+                <div class="z-10">
+                    <p class="text-4xl font-bold max-md:text-2xl max-sm:text-xl">BeeKeepers Natural.</p>
+                    <p class="pt-2 w-[600px] text-lg max-md:text-sm max-sm:text-sm max-sm:w-72 ">Subheading that sets up context, shares more info about the website, or generally gets people psyched to keep scrolling.</p>
+                    <button class="bg-yellow-500 px-3 py-1.5 text-xl mt-5 rounded font-medium leading-none max-md:px-3 max-md:py-1 max-md:text-sm max-sm:text-sm max-sm:px-2 max-sm:py-1">SignUp</button>
                 </div>
             </div>
 
-            <div class="p-4 mt-5">
-                <p class="my-2 text-3xl font-medium">Section heading</p>
-                <div class="flex justify-between py-5">
-                    <div class="border ">
+            <div class="p-4 mt-5 max-md:p-2 max-md:mt-2 max-sm:p-2 max-sm:mt-2">
+                <p class="my-2 text-3xl font-medium max-md:text-2xl max-sm:text-xl">Section heading</p>
+                <div class="flex justify-between py-5 max-md:py-3 max-md:flex-wrap max-md:gap-5 max-md:justify-around max-sm:py-2 max-sm:flex-wrap max-sm:gap-5 max-sm:justify-center">
+                    <div class="border">
                         <div>
-                            <img src="/public/images/product/Image.jpg" alt="" class="h-60">
+                            <img src="/public/images/product/Image.jpg" alt="" class="h-60 max-md:h-36 max-sm:h-40">
                         </div>
-                        <div class="w-48 p-1">
-                            <p class="py-1 text-lg">Subheading</p>
+                        <div class="w-48 p-1 max-md:w-36 max-sm:w-40">
+                            <p class="py-1 font-medium text-lg max-md:text-sm max-sm:text-sm">Subheading</p>
+                            <p class="text-xs">Body text for whatever you’d like to add more to the subheading.</p>
+                        </div>
+                    </div>
+                    <div class="border">
+                        <div>
+                            <img src="/public/images/product/Image (1).jpg" alt="" class="h-60 max-md:h-36 max-sm:h-40">
+                        </div>
+                        <div class="w-48 p-1 max-md:w-36 max-sm:w-40">
+                            <p class="py-1 font-medium text-lg max-md:text-sm max-sm:text-sm">Subheading</p>
                             <p class="text-xs">Body text for whatever you’d like to add more to the subheading.</p>
                         </div>
                     </div>
                     <div class="border ">
                         <div>
-                            <img src="/public/images/product/Image (1).jpg" alt="" class="h-60">
+                            <img src="/public/images/product/Image (2).jpg" alt="" class="h-60 max-md:h-36 max-sm:h-40">
                         </div>
-                        <div class="w-48 p-1">
-                            <p class="py-1 text-lg">Subheading</p>
+                        <div class="w-48 p-1 max-md:w-36 max-sm:w-40">
+                            <p class="py-1 font-medium text-lg max-md:text-sm max-sm:text-sm">Subheading</p>
                             <p class="text-xs">Body text for whatever you’d like to add more to the subheading.</p>
                         </div>
                     </div>
                     <div class="border ">
                         <div>
-                            <img src="/public/images/product/Image (2).jpg" alt="" class="h-60">
+                            <img src="/public/images/product/Image (3).jpg" alt="" class="h-60 max-md:h-36 max-sm:h-40">
                         </div>
-                        <div class="w-48 p-1">
-                            <p class="py-1 text-lg">Subheading</p>
-                            <p class="text-xs">Body text for whatever you’d like to add more to the subheading.</p>
-                        </div>
-                    </div>
-                    <div class="border ">
-                        <div>
-                            <img src="/public/images/product/Image (3).jpg" alt="" class="h-60">
-                        </div>
-                        <div class="w-48 p-1">
-                            <p class="py-1 text-lg">Subheading</p>
+                        <div class="w-48 p-1 max-md:w-36 max-sm:w-40">
+                            <p class="py-1 font-medium text-lg max-md:text-sm max-sm:text-sm">Subheading</p>
                             <p class="text-xs">Body text for whatever you’dnp like to add more to the subheading.</p>
                         </div>
                     </div>
                 </div>
             </div>
 
-            <div class="flex items-center justify-between p-4 mt-5">
-                <div class="w-[45%]">
-                    <p class="my-2 text-3xl font-medium">Section heading</p>
+            <p class="max-sm:mt-2 max-sm:p-2 max-sm:text-xl max-sm:font-medium max-sm:block hidden">Section heading</p>
+            <div class="flex items-center justify-between p-4 mt-5 max-md:gap-5 max-md:p-2 max-md:mt-2 max-sm:p-2 max-sm:mt-2 max-sm:flex-col-reverse">
+                <div class="w-[45%] max-md:w-96 max-sm:w-full">
+                    <p class="my-2 text-3xl font-medium max-md:text-2xl max-sm:hidden">Section heading</p>
                     <div class="mt-4">
-                        <p class="pb-1 text-lg font-medium">subheading</p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic voluptas magni sequi, Nesciunt facilis aspernatur minima magni explicabo.</p>
+                        <p class="pb-1 text-lg font-medium max-md:text-md max-sm:text-md ">subheading</p>
+                        <p class="max-md:text-sm max-sm:text-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic voluptas magni sequi, Nesciunt facilis aspernatur minima magni explicabo.</p>
                     </div>
                     <div class="mt-4">
-                        <p class="pb-1 text-lg font-medium">subheading</p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt facilis aspernatur minima magni explicabo, nemo consectetur enim eius in culpa sequi, vel pariatur, sunt similique atque nisi animi iure natus?</p>
+                        <p class="pb-1 text-lg font-medium max-md:text-2xl max-sm:text-md ">subheading</p>
+                        <p class="max-md:text-sm max-sm:text-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt facilis aspernatur minima magni explicabo, nemo consectetur enim eius in culpa sequi, vel pariatur, sunt similique atque nisi animi iure natus?</p>
                     </div>
                     <div class="mt-4">
-                        <p class="pb-1 text-lg font-medium">subheading</p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium dolore eos, id voluptates, quisquam ab necessitatibus voluptate corporis enim accusantium, cupiditate officiis consectetur perferendis vitae. Nesciunt distinctio molestias quas asperiores.</p>
+                        <p class="pb-1 text-lg font-medium max-md:text-2xl max-sm:text-md ">subheading</p>
+                        <p class="max-md:text-sm max-sm:text-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium dolore eos, id voluptates, quisquam ab necessitatibus voluptate corporis enim accusantium, cupiditate officiis consectetur perferendis vitae. Nesciunt distinctio molestias quas asperiores.</p>
                     </div>
-                    <div class="mt-5">
+                    <div class="mt-5 max-sm:mt-8">
                         <button class="px-4 py-0.5 bg-black text-white rounded text-center">Button</button>
                         <button class="px-4 py-0.5 bg-gray-300 text-black rounded text-center mx-2">Secondary button</button>
                     </div>
                 </div>
                 <div>
-                    <img src="/public/images/banners/farm.jpg" alt="" class="h-96">
+                    <img src="/public/images/banners/farm.jpg" alt="" class="h-96 max-md:h-80 max-md:w-80 max-sm:h-60">
                 </div>
             </div>
 
-            <div class="p-4 mt-5">
-                <p class="my-2 text-3xl font-medium">Section heading</p>
-                <div class="flex justify-between pt-4">
-                    <div class="border rounded">
-                        <img src="/public/images/banners/CG4B5656.jpg" alt="" class="h-56 rounded-t">
-                        <div class="p-1 w-80">
-                            <p class="text-lg font-medium">Subheading</p>
-                            <p class="py-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis architecto qui at asperiores nostrum repellendus.</p>
+            <div class="p-4 mt-5 max-md:p-2 max-md: max-sm:p-2 max-sm:mt-2">
+                <p class="my-2 text-3xl font-medium max-md:text-2xl max-sm:text-xl">Section heading</p>
+                <div class="flex justify-between pt-4 max-md:overflow-x-scroll max-md:pb-2 max-md:gap-5 max-sm:flex-col max-sm:gap-5">
+                    <div class="border rounded max-md:max-w-72 max-sm:min-w-full">
+                        <img src="/public/images/banners/CG4B5656.jpg" alt="" class="h-56 rounded-t w-full max-md:h-44 max-md:max-w-72 max-sm:min-w-full max-sm:h-48">
+                        <div class="p-1 w-80 max-md:min-w-full max-sm:w-full">
+                            <p class="text-lg font-medium max-md:text-md max-sm:text-md">Subheading</p>
+                            <p class="py-2 max-md:py-1 max-md:text-sm max-sm:text-sm max-sm:py-1">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis architecto qui at asperiores nostrum repellendus.</p>
                         </div>
                     </div>
-                    <div class="border rounded">
-                        <img src="/public/images/banners/CG4B5656.jpg" alt="" class="h-56 rounded-t">
-                        <div class="p-1 w-80">
-                            <p class="text-lg font-medium">Subheading</p>
-                            <p class="py-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis architecto qui at asperiores nostrum repellendus.</p>
+                    <div class="border rounded max-md:max-w-72 max-sm:min-w-full">
+                        <img src="/public/images/banners/CG4B5656.jpg" alt="" class="h-56 rounded-t w-full max-md:h-44 max-md:max-w-72 max-sm:min-w-full max-sm:h-48">
+                        <div class="p-1 w-80 max-md:min-w-full max-sm:w-full">
+                            <p class="text-lg font-medium max-md:text-md max-sm:text-md">Subheading</p>
+                            <p class="py-2 max-md:py-1 max-md:text-sm max-sm:text-sm max-sm:py-1">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis architecto qui at asperiores nostrum repellendus.</p>
                         </div>
                     </div>
-                    <div class="border rounded">
-                        <img src="/public/images/banners/CG4B5656.jpg" alt="" class="h-56 rounded-t">
-                        <div class="p-1 w-80">
-                            <p class="text-lg font-medium">Subheading</p>
-                            <p class="py-2">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis architecto qui at asperiores nostrum repellendus.</p>
+                    <div class="border rounded max-md:max-w-72 max-sm:min-w-full">
+                        <img src="/public/images/banners/CG4B5656.jpg" alt="" class="h-56 rounded-t w-full max-md:h-44 max-md:max-w-72 max-sm:min-w-full max-sm:h-48">
+                        <div class="p-1 w-80 max-md:min-w-full max-sm:w-full">
+                            <p class="text-lg font-medium max-md:text-md max-sm:text-md">Subheading</p>
+                            <p class="py-2 max-md:py-1 max-md:text-sm max-sm:text-sm max-sm:py-1">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis architecto qui at asperiores nostrum repellendus.</p>
                         </div>
                     </div>
                 </div>
             </div>
 
-            <div class="p-4 mt-5">
-                <p class="my-2 text-3xl font-medium">Section heading</p>
-                <div class="flex justify-between pt-4">
-                    <div class="border w-80">
-                        <p class="p-2 mt-2">“A terrific piece of praise”</p>
+            <div class="p-4 mt-5 max-md:p-2 max-md:mt-2 max-sm:p-2 max-sm:mt-2">
+                <p class="my-2 text-3xl font-medium max-md:text-2xl max-sm:text-xl">Section heading</p>
+                <div class="flex justify-between pt-4 max-md:overflow-x-scroll max-md:pb-2 max-md:gap-10 max-sm:gap-5 max-sm:overflow-x-scroll max-sm:pb-2 ">
+                    <div class="border w-80 shadow max-md:min-w-80 max-sm:min-w-56">
+                        <p class="p-2 mt-2 max-sm:text-sm">“A terrific piece of praise”</p>
                         <div class="flex gap-5 p-3">
-                            <img src="" alt="" class="w-12 h-12 border rounded-full">
+                            <img src="" alt="" title="Bharath" class="w-12 h-12 border rounded-full max-md:w-10 max-md:h-10 max-sm:w-8 max-sm:h-8">
                             <div class="">
-                                <p>Name</p>
-                                <p class="text-sm text-gray-500">Description</p>
+                                <p class="max-sm:text-xs">Name</p>
+                                <p class="text-sm text-gray-500 max-sm:text-xs">Description</p>
                             </div>
                         </div>
                     </div>
-                    <div class="border w-80">
-                        <p class="p-2 mt-2">“A terrific piece of praise”</p>
+                    <div class="border w-80 shadow max-md:min-w-80 max-sm:min-w-56">
+                        <p class="p-2 mt-2 max-sm:text-sm">“A terrific piece of praise”</p>
                         <div class="flex gap-5 p-3">
-                            <img src="" alt="" class="w-12 h-12 border rounded-full">
+                            <img src="" alt="" title="Tamil" class="w-12 h-12 border rounded-full max-md:w-10 max-md:h-10 max-sm:w-8 max-sm:h-8">
                             <div class="">
-                                <p>Name</p>
-                                <p class="text-sm text-gray-500">Description</p>
+                                <p class="max-sm:text-xs">Name</p>
+                                <p class="text-sm text-gray-500 max-sm:text-xs">Description</p>
                             </div>
                         </div>
                     </div>
-                    <div class="border w-80">
-                        <p class="p-2 mt-2">“A terrific piece of praise”</p>
+                    <div class="border w-80 shadow max-md:min-w-80 max-sm:min-w-56">
+                        <p class="p-2 mt-2 max-sm:text-sm">“A terrific piece of praise”</p>
                         <div class="flex gap-5 p-3">
-                            <img src="" alt="" class="w-12 h-12 border rounded-full">
+                            <img src="" alt="" title="Shivaram" class="w-12 h-12 border rounded-full max-md:w-10 max-md:h-10 max-sm:w-8 max-sm:h-8">
                             <div class="">
-                                <p>Name</p>
-                                <p class="text-sm text-gray-500">Description</p>
+                                <p class="max-sm:text-xs">Name</p>
+                                <p class="text-sm text-gray-500 max-sm:text-xs">Description</p>
                             </div>
                         </div>
                     </div>
@@ -142,11 +144,11 @@
             </div>    
         </div>
         
-        <div class="flex items-center justify-between p-10 mt-5 bg-gray-200">
-            <p class="my-2 text-3xl font-medium">Section heading</p>
-            <div class="flex gap-5">
+        <div class="flex items-center justify-between p-10 mt-5 bg-gray-200 max-sm:block max-sm:p-5 max-sm:mt-2">
+            <p class="my-2 text-3xl font-medium max-sm:text-xl max-sm:mb-5 max-sm:text-center">Section heading</p>
+            <div class="flex gap-5 max-sm:gap-2 max-sm:justify-center">
                 <input type="email" placeholder="Email" class="px-1 ">
-                <button class="px-4 py-0.5 rounded bg-black text-white">Submit</button>
+                <button class="px-4 py-0.5 rounded bg-black text-white max-sm:px-2 max-sm:py-0 max-sm:text-sm">Submit</button>
             </div>
         </div>
         
