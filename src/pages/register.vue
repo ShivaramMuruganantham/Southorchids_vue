eb57d41e9e38419fb32d9fae57f3e63bb56279af
Shivaram <144917260+ShivaramMuruganantham@users.noreply.github.com>
2025-01-24 14:35:10 +0530

    Responsive register page

--- src/pages/register.vue
+++ src/pages/register.vue
@@ -1,61 +1,62 @@
 <template>
-    <div class="p-5">
-        <div class="relative flex justify-center">
-            <p class="absolute text-3xl left-64" @click="$router.go(-1)"><i class="icon ion-md-arrow-back"></i></p>
-            <p class="text-4xl ">Sign up</p>
+    <div class="p-5 max-sm:p-5">
+        <div class="relative flex justify-between mx-10 max-sm:justify-between  max-sm:mx-3 max-sm:items-center">
+            <p class="text-3xl max-sm:text-2xl " @click="$router.go(-1)"><i class="icon ion-md-arrow-back"></i></p>
+            <p class="text-4xl max-sm:text-2xl max-sm:font-bold">Sign up</p>
+            <p class="max-sm:w-5"></p>
         </div>
-        <div class="flex justify-center my-8">
-            <form class="border-2 border-l-yellow-400 shadow rounded py-4 w-[60%]">
-                <div class="flex justify-around ">
+        <div class="flex justify-center my-8 ">
+            <form class="border-2 border-l-yellow-400 shadow rounded py-4 w-[60%] max-sm:w-full max-sm:p-8">
+                <div class="flex justify-around max-sm:flex-col">
                     <div>
-                        <p class="py-1 text-sm">Name :</p>
-                        <input type="text" v-model="form.name" name="name" @blur="NameValidation(form.name)" class="p-1 text-sm border border-black rounded outline-none focus:border-2 focus:border-green-500">
-                        <p class="py-1 text-xs text-red-500 w-36">{{ msg.name }}</p>
+                        <p class="py-1 max-sm:text-lg">Name :</p>
+                        <input type="text" v-model="form.name" name="name" @blur="NameValidation(form.name)" class="p-1 text-sm border border-black rounded outline-none focus:border-2 focus:border-green-500 max-sm:w-full">
+                        <p class="py-1 text-xs text-red-500 w-36 max-sm:text-sm max-sm:pb-1 max-sm:w-full">{{ msg.name }}</p>
                     </div>
                     <div>
-                        <p class="py-1">Email :</p>
-                        <input type="email" v-model="form.email" name="email" @blur="MailValidation(form.email)" class="p-1 text-sm border border-black rounded outline-none focus:border-2 focus:border-green-500">
-                        <p class="py-1 text-xs text-red-500 w-36">{{ msg.email }}</p>
-                    </div>
-                </div>
-                <div class="flex justify-around">
-                    <div>
-                        <p class="py-1">Password :</p>
-                        <input type="password" v-model="form.password" name="password" @blur="PasswordValidation(form.password)" class="p-1 text-sm border border-black rounded outline-none focus:border-2 focus:border-green-500">
-                        <p class="py-1 text-xs text-red-500 w-36">{{ msg.password }}</p>
-                    </div>
-                    <div>
-                        <p class="py-1">Confirm :</p>
-                        <input type="password" v-model="form.confirm" name="confirm" @blur="ConfirmValidation(form.confirm)" class="p-1 text-sm border border-black rounded outline-none focus:border-2 focus:border-green-500">
-                        <p class="py-1 text-xs text-red-500 w-36">{{ msg.confirm }}</p>
+                        <p class="py-1 max-sm:text-lg">Email :</p>
+                        <input type="email" v-model="form.email" name="email" @blur="MailValidation(form.email)" class="p-1 text-sm border border-black rounded outline-none focus:border-2 focus:border-green-500 max-sm:w-full">
+                        <p class="py-1 text-xs text-red-500 w-36 max-sm:text-sm max-sm:pb-1 max-sm:w-full">{{ msg.email }}</p>
                     </div>
                 </div>
-                <div class="flex justify-around">
+                <div class="flex justify-around max-sm:flex-col">
                     <div>
-                        <p class="py-1">Address :</p>
-                        <textarea v-model="form.address" name="address" rows="3" @blur="AddressValidation(form.address)" class="p-1 text-sm border border-black rounded outline-none focus:border-2 focus:border-green-500"></textarea>
-                        <p class="py-1 text-xs text-red-500 w-36">{{ msg.address }}</p>
+                        <p class="py-1 max-sm:text-lg">Password :</p>
+                        <input type="password" v-model="form.password" name="password" @blur="PasswordValidation(form.password)" class="p-1 text-sm border border-black rounded outline-none focus:border-2 focus:border-green-500 max-sm:w-full">
+                        <p class="py-1 text-xs text-red-500 w-36 max-sm:text-sm max-sm:pb-1 max-sm:w-full">{{ msg.password }}</p>
                     </div>
                     <div>
-                        <p class="py-1">Phone :</p>
-                        <input type="number" v-model="form.phone" name="phone" @blur="PhoneValidation(form.phone)" class="p-1 text-sm border border-black rounded outline-none focus:border-2 focus:border-green-500">
-                        <p class="py-1 text-xs text-red-500 w-36">{{ msg.phone }}</p>
+                        <p class="py-1 max-sm:text-lg">Confirm :</p>
+                        <input type="password" v-model="form.confirm" name="confirm" @blur="ConfirmValidation(form.confirm)" class="p-1 text-sm border border-black rounded outline-none focus:border-2 focus:border-green-500 max-sm:w-full">
+                        <p class="py-1 text-xs text-red-500 w-36 max-sm:text-sm max-sm:pb-1 max-sm:w-full">{{ msg.confirm }}</p>
                     </div>
                 </div>
-                <div class="flex justify-around">
+                <div class="flex justify-around max-sm:flex-col">
                     <div>
-                        <p class="py-1">City :</p>
-                        <input type="text" v-model="form.city" name="city" @blur="CityValidation(form.city)" class="p-1 text-sm border border-black rounded outline-none focus:border-2 focus:border-green-500">
-                        <p class="py-1 text-xs text-red-500 w-36">{{ msg.city }}</p>
+                        <p class="py-1 max-sm:text-lg">Address :</p>
+                        <textarea v-model="form.address" name="address" rows="3" @blur="AddressValidation(form.address)" class="p-1 text-sm border border-black rounded outline-none focus:border-2 focus:border-green-500 max-sm:w-full"></textarea>
+                        <p class="py-1 text-xs text-red-500 w-36 max-sm:text-sm max-sm:pb-1 max-sm:w-full">{{ msg.address }}</p>
                     </div>
                     <div>
-                        <p class="py-1">Pincode :</p>
-                        <input type="number" v-model="form.pincode" name="pincode" @blur="PincodeValidation(form.pincode)" class="p-1 text-sm border border-black rounded outline-none focus:border-2 focus:border-green-500">
-                        <p class="py-1 text-xs text-red-500 w-36">{{ msg.pincode }}</p>
+                        <p class="py-1 max-sm:text-lg">Phone :</p>
+                        <input type="number" v-model="form.phone" name="phone" @blur="PhoneValidation(form.phone)" class="p-1 text-sm border border-black rounded outline-none focus:border-2 focus:border-green-500 max-sm:w-full">
+                        <p class="py-1 text-xs text-red-500 w-36 max-sm:text-sm max-sm:pb-1 max-sm:w-full">{{ msg.phone }}</p>
+                    </div>
+                </div>
+                <div class="flex justify-around max-sm:flex-col">
+                    <div>
+                        <p class="py-1 max-sm:text-lg">City :</p>
+                        <input type="text" v-model="form.city" name="city" @blur="CityValidation(form.city)" class="p-1 text-sm border border-black rounded outline-none focus:border-2 focus:border-green-500 max-sm:w-full">
+                        <p class="py-1 text-xs text-red-500 w-36 max-sm:text-sm max-sm:pb-1 max-sm:w-full">{{ msg.city }}</p>
+                    </div>
+                    <div>
+                        <p class="py-1 max-sm:text-lg">Pincode :</p>
+                        <input type="number" v-model="form.pincode" name="pincode" @blur="PincodeValidation(form.pincode)" class="p-1 text-sm border border-black rounded outline-none focus:border-2 focus:border-green-500 max-sm:w-full">
+                        <p class="py-1 text-xs text-red-500 w-36 max-sm:text-sm max-sm:pb-1 max-sm:w-full">{{ msg.pincode }}</p>
                     </div>
                 </div>
                 <div class="flex justify-center py-5">
-                    <button @click.prevent="SubmitForm()" class="border border-black rounded px-5 py-1.5 focus:border-2 focus:border-green-500 outline-none">Submit</button>
+                    <button @click.prevent="SubmitForm()" class="bg-yellow-500 text-white font-medium shadow-lg rounded px-5 py-1.5 focus:border-2 focus:border-green-500 outline-none max-sm:text-lg max-sm:pb-1">Submit</button>
                 </div>
                 <p></p>
             </form>
