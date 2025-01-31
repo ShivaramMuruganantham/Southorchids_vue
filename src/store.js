51806fbf7ff3582fe6f2cefa61eae8dc054fc9bf
Shivaram <144917260+ShivaramMuruganantham@users.noreply.github.com>
2025-01-31 16:11:12 +0530

    Config API_URL in every page

--- src/store.js
+++ src/store.js
@@ -5,6 +5,7 @@ export const useStore = defineStore('Orders', {
     state: () => ({
         count : 0,
         orders : [],
+        api_url: 'http://localhost:8000/api',
     }),
 
     actions: {
