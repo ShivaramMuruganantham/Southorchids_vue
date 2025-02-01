3001bbb1b2601ecb3e86c47d174ed64f5f3aae75
Shivaram <144917260+ShivaramMuruganantham@users.noreply.github.com>
2025-02-01 15:09:33 +0530

    Connect to backend

--- src/store.js
+++ src/store.js
@@ -5,7 +5,7 @@ export const useStore = defineStore('Orders', {
     state: () => ({
         count : 0,
         orders : [],
-        api_url: 'http://localhost:8000/api',
+        api_url: 'http://13.201.122.152/api',
     }),
 
     actions: {
