11434cb62d27820d65f6b4fb1d6c55f2a7eac5d3
Shivaram <144917260+ShivaramMuruganantham@users.noreply.github.com>
2025-02-11 10:49:06 +0530

    domain change

--- src/store.js
+++ src/store.js
@@ -5,7 +5,8 @@ export const useStore = defineStore('Orders', {
     state: () => ({
         count : 0,
         orders : [],
-        api_url: 'http://13.201.122.152/api',
+        api_url: 'https://beeprotector.publicvm.com/api',
+        // api_url: 'http://localhost:8000/api',
     }),
 
     actions: {
